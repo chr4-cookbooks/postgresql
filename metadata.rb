6ebfdcb93d9f728885ed743405596de27874053f
Chris Aumann <me@chr4.org>
2016-06-09 16:15:38 +0200

    Add source_url and issues_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/chr4-cookbooks/postgresql' if respond_to?(:source_url)
+issues_url       'https://github.com/chr4-cookbooks/postgresql/issues' if respond_to?(:issues_url)
 version          '1.1.0'
 depends          'certificate'
 depends          'build-essential'
